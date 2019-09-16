651f01860aba585338e56692423695323c79d174
thbisdikian <theo.bisdikian@gmail.com>
2019-09-16 16:06:24 -0400

    added main grid, expanded class hierarchy for bottom app bar and sound grid widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Noiseio());
 
-class MyApp extends StatelessWidget {
+class Noiseio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,11 +28,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -41,72 +36,106 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        // title: Text(widget.title),
+        title: Text('$_counter'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.grey,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                height: 100,
-                child: FlatButton(
-                  child: Icon(Icons.play_arrow, size: 60.0),
-                  onPressed:  () => _incrementCounter(),
-                ),
-              ),
-            ),
-            Container(
+      body: SoundGrid(),
+      bottomNavigationBar: BottomBar(onPressed: _incrementCounter,),
+    );
+  }
+}
+
+class SoundGrid extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return GridView.count(
+      primary: true,
+      crossAxisCount: 2,
+      childAspectRatio: 1.5,
+      children: <Widget>[
+        SoundButton(color: Colors.blueGrey[800],),
+        SoundButton(color: Colors.blue[900],),
+        SoundButton(color: Colors.lightBlue[900],),
+        SoundButton(color: Colors.indigo[800],),
+        SoundButton(color: Colors.teal[900],),
+        SoundButton(color: Colors.cyan[900],),
+      ],
+    );
+  }
+}
+
+class SoundButton extends StatefulWidget {
+  SoundButton({Key key, this.color}) : super(key: key);
+  final Color color;
+
+  @override
+  SoundButtonState createState() => SoundButtonState();
+}
+
+class SoundButtonState extends State<SoundButton> {
+  bool turnedOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container (
+      color: widget.color,
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar ({this.onPressed});
+
+  final VoidCallback onPressed;
+  
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.grey,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Container(
               height: 100,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[700]),
+              child: FlatButton(
+                child: Icon(Icons.play_arrow, size: 60.0),
+                onPressed:  () => onPressed(),
               ),
             ),
-            Expanded(
-              child: Container(
-                height: 100,
-                child: FlatButton(
-                  child: Icon(Icons.timer, size: 60.0),
-                  onPressed:  () => _incrementCounter(),
-                ),
-              ),
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey[700]),
             ),
-            Container(
+          ),
+          Expanded(
+            child: Container(
               height: 100,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[700]),
+              child: FlatButton(
+                child: Icon(Icons.timer, size: 60.0),
+                onPressed:  () => onPressed(),
               ),
             ),
-            Expanded(
-              child: Container(
-                height: 100,
-                child: FlatButton(
-                  child: Icon(Icons.favorite_border, size: 60.0),
-                  onPressed:  () => _incrementCounter(),
-                ),
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey[700]),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 100,
+              child: FlatButton(
+                child: Icon(Icons.favorite_border, size: 60.0),
+                onPressed:  () => onPressed(),
               ),
             ),
-          ],
-        )
-      ),
+          ),
+        ],
+      )
     );
   }
 }
