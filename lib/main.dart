c69f9769cdaff05ef139ede2bb713458ddfbe27d
thbisdikian <theo.bisdikian@gmail.com>
2019-09-18 16:40:36 -0400

    Added timer with basic async functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:countdown/countdown.dart';
 
 void main() => runApp(Noiseio());
 
@@ -16,8 +19,9 @@ class Noiseio extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -25,6 +29,37 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool playing = false;
+  CountDown countdown;
+  StreamSubscription<Duration> countdownListener;
+  Duration remaining;
+
+  _timerSet(Duration dur) {
+    if (countdown != null) {
+      countdownListener.cancel();
+    }
+    countdown = CountDown(dur, refresh: Duration(minutes:1));
+    countdownListener = countdown.stream.listen(null);
+    countdownListener.onData((Duration d) => remaining = d);
+    countdownListener.onDone(() => playing = false);
+  }
+  
+
+  _playPause() {
+    playing ? _pause() : _play();
+  }
+
+  _play() {
+    setState(() {
+     playing = true; 
+    });
+  }
+
+  _pause() {
+    setState(() {
+     playing = false; 
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -34,13 +69,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         // title: Text(widget.title),
-        title: Text('$_counter'),
+        title: Text('$_counter $playing'),
+        // title: Text("${remaining.inHours%60}".padLeft(2, "0") + ":" + "${remaining.inMinutes%60}".padLeft(2, "0")),
       ),
       body: SoundGrid(),
-      bottomNavigationBar: BottomBar(tempBottomFunction: _incrementCounter,),
+      bottomNavigationBar: BottomBar(parent: this ,tempBottomFunction: _incrementCounter,),
     );
   }
 }
@@ -64,8 +101,9 @@ class SoundGrid extends StatelessWidget {
 }
 
 class SoundButton extends StatefulWidget {
-  SoundButton({Key key, this.color}) : super(key: key);
   final Color color;
+
+  SoundButton({Key key, this.color}) : super(key: key);
 
   @override
   SoundButtonState createState() => SoundButtonState();
@@ -118,9 +156,11 @@ class SoundButtonState extends State<SoundButton> {
 }
 
 class BottomBar extends StatelessWidget {
-  const BottomBar ({this.tempBottomFunction});
 
+  final _MyHomePageState parent;
   final VoidCallback tempBottomFunction;
+
+  BottomBar({this.parent, this.tempBottomFunction});
   
   Widget build(BuildContext context) {
     return BottomAppBar(
@@ -132,45 +172,93 @@ class BottomBar extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: Container(
-              height: 100,
+              height: 80,
               child: FlatButton(
-                child: Icon(Icons.play_arrow, size: 60.0),
-                onPressed:  () => tempBottomFunction(),
+                child: Icon(parent.playing ? Icons.pause : Icons.play_arrow, size: 40),
+                onPressed:  () => parent._playPause(),
               ),
             ),
           ),
           Container(
-            height: 100,
+            height: 80,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey[700]),
+            ),
+          ),
+          CountdownTimer(parent: parent),
+          Container(
+            height: 80,
             decoration: BoxDecoration(
               border: Border.all(color: Colors.grey[700]),
             ),
           ),
           Expanded(
             child: Container(
-              height: 100,
+              height: 80,
               child: FlatButton(
-                child: Icon(Icons.timer, size: 60.0),
-                onPressed:  () => tempBottomFunction(),
-              ),
-            ),
-          ),
-          Container(
-            height: 100,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[700]),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              height: 100,
-              child: FlatButton(
-                child: Icon(Icons.favorite_border, size: 60.0),
+                child: Icon(Icons.favorite_border, size: 40),
                 onPressed:  () => tempBottomFunction(),
               ),
             ),
           ),
         ],
       )
+    );
+  }
+}
+
+class CountdownTimer extends StatefulWidget {
+  final _MyHomePageState parent;
+
+  CountdownTimer({this.parent});
+
+  @override
+  CountdownTimerState createState() => CountdownTimerState();
+}
+
+class CountdownTimerState extends State<CountdownTimer> {
+  bool counting = false;
+  CountDown cd;
+  StreamSubscription<Duration> sub;
+  Duration r = Duration();
+
+  _countfrom10() {
+    setState(() {
+      if(counting) {
+        sub.cancel();
+      }
+      counting = true;
+      cd = CountDown(Duration(seconds:10), refresh: Duration(milliseconds: 500));
+      sub = cd.stream.listen(null);
+
+      sub.onData((Duration d) {
+        setState(() {
+          r = d;
+        });
+      });
+
+
+      sub.onDone(() {
+        setState(() {
+          counting = false;
+        });
+        widget.parent._pause();
+      });
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 80,
+        child: FlatButton(
+          child: counting ? 
+            Text("${r.inMinutes%60}".padLeft(2, "0") + ":" + "${(r.inSeconds)%60}".padLeft(2, "0")) : 
+            Icon(Icons.timer, size: 40),
+          onPressed:  () => _countfrom10(),
+        ),
+      ),
     );
   }
 }
